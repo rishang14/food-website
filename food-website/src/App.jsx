6f9799b8065eac52cb13710d6c71c38744471455
Rishang <rishangkumar9693@gmail.com>
2023-11-15 10:38:06 +0530

    added meal section and many more

--- food-website/src/App.jsx
+++ food-website/src/App.jsx
@@ -1,12 +1,16 @@
 import TopNav from "./components/TopNav";
 import Featured from "./components/Featured"; 
-import Delivery from "./components/Delivery";
+import Delivery from "./components/Delivery"; 
+import TopPicks from "./components/TopPicks"; 
+import Meal from "./components/Meal";
 const App = () => {
   return (
     <>
       <TopNav />
       <Featured /> 
-      <Delivery/>
+      <Delivery/> 
+      <TopPicks/> 
+      <Meal/>
     </>
   );
 };
--- food-website/src/components/Delivery.jsx
+++ food-website/src/components/Delivery.jsx
@@ -1,14 +1,13 @@
 const Delivery = () => {
- return(
-  <>
+ return (
   <div className="w-full bg-white py-16 px-4">
     <h3 className="text-orange-500 font-bold text-center text-2xl">Quick Delivery App</h3> 
-    <div className="w-[1240px] mx-auto grid md:grid-cols-2">
-      <img className="w-[550px] mx-auto my-4" src="https://res.cloudinary.com/ehizeex-shop/image/upload/v1672676822/NetflixApp/FC_two_phones.6ec9a842f905769677f9_m91off.webp" /> 
-      <div className="flex flex-col justify-center"> 
-      <p className="text-[#00df9a] font-bold">Get The App</p> 
-      <h1 className="text-2xl md:text-4xl sm:text-3xl py-2 font-bold">Limitless Covenience on-demand</h1> 
-      <p>  Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+    <div className="lg:w-[1240px] mx-auto lg:flex flex-row ">
+      <img className=" w-[550px]  mx-auto my-4" src="https://res.cloudinary.com/ehizeex-shop/image/upload/v1672676822/NetflixApp/FC_two_phones.6ec9a842f905769677f9_m91off.webp" /> 
+      <div className="flex flex-col justify-center flex-wrap w-[350px] md:w-[100%]"> 
+      <p className="text-[#00df9a] font-bold sm:flex sm:justify-center">Get The App</p> 
+      <h1 className="text-2xl md:text-4xl sm:text-3xl py-2 font-bold sm:text-center">Limitless Covenience on-demand</h1> 
+      <p  className="flex justify-center items-center sm:text-center " >  Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
             molestiae delectus culpa hic assumenda, voluptate reprehenderit
             dolore autem cum ullam sed odit perspiciatis. Doloribus quos velit,
             eveniet ex deserunt fuga? Lorem ipsum dolor sit amet consectetur
@@ -19,11 +18,10 @@ const Delivery = () => {
             molestiae delectus culpa hic assumenda, voluptate reprehenderit
             dolore autem cum ullam sed odit perspiciatis. Doloribus quos velit,
             eveniet ex deserunt fuga?</p> 
-            <button className="bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3">Get Started</button>
+            <button className="bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-auto py-3 ">Get Started</button>
       </div>
     </div>
   </div> 
-  </> 
  )
 }
 
