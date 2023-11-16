3b20c2995349637248ebfb6ff4c30d51a4115a80
Rishang <rishangkumar9693@gmail.com>
2023-11-16 16:20:39 +0530

    changes in the main branch made

--- food-website/src/App.jsx
+++ food-website/src/App.jsx
@@ -1,29 +1,11 @@
 import TopNav from "./components/TopNav";
-// import Featured from "./components/Featured";
-// import Delivery from "./components/Delivery";
-// import TopPicks from "./components/TopPicks";
-// import Meal from "./components/Meal";
-// import Cateogries from "./components/Cateogries";
-// import NewsLetter from "./components/NewsLetter"; 
-// import Footer from "./components/Footer"; 
 import CustomRoutes from "./components/CustomRoutes";
-
-
 
 const App = () => {
   return (
-    <> 
-    <TopNav/>  
-    <CustomRoutes/>
-        {/* <Featured />
-      <Delivery />
-      <TopPicks />
-      <Meal />
-      <Cateogries />
-      <NewsLetter/> 
-      <Footer/>   */}
-     
-       
+    <>
+      <TopNav />
+      <CustomRoutes />
     </>
   );
 };
